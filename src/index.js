9917660e80c977ad5ae89e83c8d08aed2ce75533
Denys Semeniuk <serking3@gmail.com>
2025-10-16 13:46:59 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-'use strict';
+const app = require('./app');
+const dotenv = require('dotenv');
+const { sequelize } = require('./db');
+
+require('./models/User');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    // eslint-disable-next-line no-console
+    console.log('Connected to PostgreSQL');
+
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to DB:', error.message);
+  }
+})();
